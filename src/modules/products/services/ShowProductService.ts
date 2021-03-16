02c23617dbbfb59dff856aaaebc7a49ca5c2216a
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-16 19:14:06 -0300

    Criação de serviço para pesquisar por um ID

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/errors/AppErrors';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+interface ShowProductRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({
+    id,
+  }: ShowProductRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
