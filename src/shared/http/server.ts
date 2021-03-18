866a3120fe5ad163d78be20d0466beed97fc4a68
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-18 19:42:36 -0300

    importando o celebrate para retornar o error com use.app

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppErrors';
 import '@shared/typeorm';
@@ -13,7 +14,10 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use(errors());
+
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
