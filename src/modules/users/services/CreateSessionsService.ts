2d3478545008355a80d22d4a6841a7cb2a1ea1a4
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-19 18:37:55 -0300

    uma const token com metodo sign de jsonwebtoken, com as opções de hash, subject e uma data de expiração

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppErrors';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
@@ -10,12 +11,12 @@ interface UserRequest {
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
-
+interface IResponse {
+  user: User;
+  token: string;
+}
 class CreateSessionsService {
-  public async execute({ email, password }: UserRequest): Promise<User> {
+  public async execute({ email, password }: UserRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -29,7 +30,15 @@ class CreateSessionsService {
       throw new AppError('Incoreect email/password combination.', 401);
     }
 
-    return user;
+    const token = sign({}, 'f3773c3c37eed0cab778f5d6f71b6001', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
