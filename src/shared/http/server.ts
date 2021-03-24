0de6dcec574467334967b64240c1aee0d2e55398
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-23 22:14:59 -0300

    Ceiando uma rota files para facilitar o front end a consumir os arquivos que sao salvos no uploads.

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,13 +5,14 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppErrors';
+import uploadConfig from '@config/upload';
 import '@shared/typeorm';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
