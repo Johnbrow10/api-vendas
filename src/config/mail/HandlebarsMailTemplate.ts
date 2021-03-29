c6c39f078d8a3982e62f6c9c54743cedbb30b423
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-29 17:35:53 -0300

    Mudando o template para ser usado arquivo no lugar

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,17 +1,21 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 interface ITemplateVariable {
   [key: string]: string | number;
 }
 
 interface IParse {
-  template: string;
+  file: string;
   variables: ITemplateVariable;
 }
 
 export default class HandlebarsMailTemplate {
-  public async parse({ template, variables }: IParse): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+  public async parse({ file, variables }: IParse): Promise<string> {
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf8',
+    });
+    const parseTemplate = handlebars.compile(templateFileContent);
 
     return parseTemplate(variables);
   }
