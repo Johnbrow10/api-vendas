7822c129aed68db794956dc728ebf5f10350c26d
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-03-29 21:49:04 -0300

    Criação da rota de perfil do usuario: a rota passa os campos name, email, old_password, ja os campos de password e passsword_confirmation tem as validações como referencia para password entao tem que ser igual e tbm password_confirmation e requerido quando o campo password existir pq ele e optional

---
+++ src/modules/users/routes/profile.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import ProfileController from '../controllers/ProfileController';
+import { join } from 'node:path';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(isAuthenticated);
+
+profileRouter.get('/', profileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().optional(),
+      password_confirmation: Joi.string()
+        // password confirmation tem que ter o valor igual o password
+        .valid(Joi.ref('password'))
+        // depos de confirmar ele e so requirido se a opcao de cima for vdd
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required,
+        }),
+    },
+  }),
+  profileController.update,
+);
+
+export default profileRouter;
