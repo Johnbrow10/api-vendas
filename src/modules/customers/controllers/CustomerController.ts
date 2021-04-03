72bcb3e5a93b4925687cca46c86cce76027310c8
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-03 19:10:40 -0300

    Controller para acionar os  serviços para formar um crud da migration de customers

---
+++ src/modules/customers/controllers/CustomerController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from 'express';
+import CreateCustomerService from '../services/CreateCustomerService';
+import DeleteCustomerService from '../services/DeleteCustomerService';
+import ListCustomerService from '../services/ListCustomerService';
+import ShowCustomerService from '../services/ShowCustomerService';
+import UpdateCustomerService from '../services/UpdateCustomerService';
+
+export default class CustomersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listCustomers = new ListCustomerService();
+
+    const customer = await listCustomers.execute();
+
+    return response.json(customer);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showCustomers = new ShowCustomerService();
+
+    const customer = await showCustomers.execute({ id });
+
+    return response.json(customer);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+
+    const createCustomer = new CreateCustomerService();
+
+    const customer = await createCustomer.execute({
+      name,
+      email,
+    });
+
+    return response.json(customer);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const { id } = request.params;
+
+    const customerUpdate = new UpdateCustomerService();
+
+    const customer = await customerUpdate.execute({
+      id,
+      name,
+      email,
+    });
+
+    return response.json(customer);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const customerDelete = new DeleteCustomerService();
+
+    await customerDelete.execute({ id });
+
+    return response.json([]);
+  }
+}
