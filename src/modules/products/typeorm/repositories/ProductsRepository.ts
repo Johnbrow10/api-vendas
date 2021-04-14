103dc08a0ccb981276b046d70642fbf573d79c7a
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-14 11:53:35 -0300

    add funcao de find todos os ids de produtos

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, In } from 'typeorm';
 import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
@@ -10,5 +14,19 @@ export class ProductRepository extends Repository<Product> {
       },
     });
     return product;
+  }
+
+  // achar todos os products atraves do id
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const productIds = products.map(product => product.id);
+
+    //  perocrrre todo o array a procura de todos os produtos existentes nesse array
+    const existsProducts = await this.find({
+      where: {
+        id: In(productIds),
+      },
+    });
+
+    return existsProducts;
   }
 }
