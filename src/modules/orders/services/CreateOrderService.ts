e06930efe155937a181dc1071c10c32de3060a00
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-14 11:54:35 -0300

    add servico ainda incompleto para criar pedidos de compras.

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,34 @@
+import CustomersRepository from '@modules/customers/typeorm/repositories/CustomersRepository';
+import { ProductRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+import AppError from '@shared/errors/AppErrors';
+import { getCustomRepository } from 'typeorm';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+class CreateOrderService {
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const customerExists = await customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Could not find any customer with the given id');
+    }
+
+    const existsProducts = await productsRepository.findAllByIds(products);
+  }
+}
+
+export default CreateOrderService;
