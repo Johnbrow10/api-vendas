fbeb579679e516f143c71720b40058cda8563006
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-15 17:17:24 -0300

    Substituirmos o metodo find do reṕositorio de customers e começamos a usar o createQueryBuilder() que aceita a funcao paginate() do typeorm asssim substituindo como os dados serao retornados, para o retorno funcionar, tem que criar uma interface com os campos padrões da documentação de IPaginateCustomer com a data como array dos dadso da entidade de customer

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -2,13 +2,26 @@ import { getCustomRepository } from 'typeorm';
 import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<IPaginateCustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customers = await customersRepository.find({});
+    //  Criando uma lista com pagination com o midleware de typeorm a funcao paginate
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    // E para o retorno funcionar, tem que criar uma interface com os campos padrões da documentação de IPaginateCustomer com a data como array dos dadso da entidade de customer
+    return customers as IPaginateCustomer;
   }
 }
 
