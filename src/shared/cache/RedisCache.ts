a2d70fa5ca44c9a22d585805d73893820fcb96b0
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-16 17:07:31 -0300

    Arquivo de redisCache para cache da aplicação

---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,20 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+
+export default class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    console.log(key, value);
+  }
+
+  //  descrevendo que o metodo e aquele tipo generico com o "T" e a promessa tbm e desse tipo o "T"
+  // public async recover<T>(key: string): Promise<T | null>{}
+
+  //  Metodo para apagar cache
+  // public async invalidate(key: string): Promise<void> {}
+}
