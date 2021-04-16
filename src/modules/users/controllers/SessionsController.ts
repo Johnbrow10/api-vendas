1c8beb4fea6607a2bf9e6aa8558941f26d349736
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-16 15:28:52 -0300

    Importamos agora a funcao de ClassToClass para retornar os dados de usuarios baseado nas alterações que a lib gfez na entidade

--- src/modules/users/controllers/SessionsController.ts
+++ src/modules/users/controllers/SessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import CreateSessionsService from '../services/CreateSessionsService';
+import { classToClass } from 'class-transformer';
 
 export default class SesionsController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -12,6 +13,6 @@ export default class SesionsController {
       password,
     });
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
