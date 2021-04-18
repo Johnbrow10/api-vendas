34775accf1ff155891aabfa476858c986881c215
Johnatan Dos Santos silva <iroman250@gmail.com>
2021-04-17 22:10:10 -0300

    Invalidando cache com redis Cache nos services que alteram os dados salvos, por exemplo atualização, criação e exclusao de dados, entao antes do save no banco de dados ele invalida o cache, apagando ele do banco de dados do redis

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -1,3 +1,4 @@
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppErrors';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
@@ -22,11 +23,15 @@ class CreateProductService {
       throw new AppError('There is alredy one product with this name');
     }
 
+    const redisCache = new RedisCache();
+
     const product = productsRepository.create({
       name,
       price,
       quantity,
     });
+
+    await redisCache.invalidate('api-vendas-PRODUCT_LIST');
 
     await productsRepository.save(product);
 
